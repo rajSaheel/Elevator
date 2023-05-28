00a5104468a1e41caffc5cc9659e7d1d99ccf52c
rajSaheel <srg12114@gmail.com>
2023-05-28 21:15:41 +0530

    fixed step call and added audio

--- script.js
+++ script.js
@@ -1,79 +1,103 @@
-const liftState = {
-    level: 2,
-    moving: false,
+const maxLevel = document.querySelectorAll(".level").length - 1
+const lift = document.querySelector("#lift")
+const timePerLevel = 5000
 
-    queue: [],
+const liftState = {
+    level: maxLevel,
+    moving: false,
 }
 
-const lift = document.querySelector("#lift")
+const filler = new Audio("./assets/filler.mp3")
+const ding = new Audio("./assets/ding.mp3")
+
+let time = 0
+let timeInterval = setInterval(() => {
+    time += 100
+}, 100)
+
+function callLift(level, direction) {
+    if (liftState.moving === false && (level == 0 || level == maxLevel)) {
+        time = 0
+        const interval = maxLevel
+        switch (direction) {
+            case "UP":
+                if (liftState.level < maxLevel) {
+                    lift.style.top = `${16 * maxLevel + 5}vw`
+                    filler.play()
+                    lift.style.animation = createAnimation(interval, direction)
+                    liftState.moving = direction
+                    for (let i = 0; i < maxLevel; i++) {
+                        globalThis.stepInterval = setTimeout(() => {
+                            liftState.level = liftState.level + 1
+                            console.log(`crossed level-${i}`)
+                        }, timePerLevel * (i + 1))
+                    }
+                }
+                break
+            case "DOWN":
+                if (liftState.level > 0) {
+                    filler.play()
+                    lift.style.top = "5vw"
+                    lift.style.animation = createAnimation(interval, direction)
+                    liftState.moving = direction
+
+                    for (let i = maxLevel; i > 0; i--) {
+                        globalThis.stepInterval = setTimeout(() => {
+                            liftState.level = liftState.level - 1
+                            console.log(`crossed level-${i}`)
+                        }, timePerLevel * (maxLevel - i + 1))
+                    }
+                }
+                break
+            default:
+        }
+        globalThis.fullInterval = setTimeout(() => {
+            filler.pause()
+            ding.play()
+            liftState.level = direction === "UP" ? maxLevel : 0
+            liftState.moving = false
+            console.log("reached extreme")
+            time = 0
+        }, timePerLevel * maxLevel)
+    } else if (liftState.moving === direction) {
+        switch (direction) {
+            case "UP":
+                console.log(time)
+                if (liftState.level <= level && timePerLevel * level > time) {
+                    setTimeout(() => {
+                        clearTimeout(globalThis.stepInterval)
+                        clearTimeout(globalThis.fullInterval)
+                        lift.style.animationPlayState = "paused"
+                        filler.pause()
+                        ding.play()
+                        console.log("pressed at" + level)
+                        time = 0
+                    }, timePerLevel * level - time)
+                }
+                break
+
+            case "DOWN":
+                console.log(time)
+                if (
+                    liftState.level >= level &&
+                    timePerLevel * (maxLevel - level) > time
+                ) {
+                    setTimeout(() => {
+                        clearTimeout(globalThis.stepInterval)
+                        clearTimeout(globalThis.fullInterval)
+                        filler.pause()
+                        ding.play()
+                        lift.style.animationPlayState = "paused"
+                        time = 0
+                    }, timePerLevel * (maxLevel - level) - time)
+                }
+                break
+        }
+    }
+}
 
 function createAnimation(interval, direction) {
     const value = `${interval * 16}vw`
     document.documentElement.style.setProperty("--transform-length", value)
     return `move-${direction} ${5 * interval}s ease-in-out 1 forwards`
-}
-let time = 0
-let timeInterval = setInterval(() => {
-    time += 100
-    // console.log(time)
-}, 100)
-function callLift(level, direction) {
-    if (liftState.moving === false && level !== 1) {
-        time = 0
-        console.log(time)
-        liftState.queue.push(level)
-        const interval = 2
-        switch (direction) {
-            case "UP":
-                if (liftState.level !== 2) {
-                    const upwardAnimation = createAnimation(interval, direction)
-                    lift.style.animation = upwardAnimation
-                    liftState.moving = direction
-                }
-
-                break
-            case "DOWN":
-                if (liftState.level !== 0) {
-                    const downwardAnimation = createAnimation(
-                        interval,
-                        direction
-                    )
-                    lift.style.animation = downwardAnimation
-                    liftState.moving = direction
-                }
-
-                break
-            default:
-        }
-
-        setTimeout(() => {
-            liftState.level = 1
-            console.log("crossed level 1")
-        }, 5000)
-        setTimeout(() => {
-            liftState.level = level
-            liftState.moving = false
-            console.log("reached extreme")
-            time = 0
-        }, 10000)
-        switch (liftState.level) {
-            case 0:
-                lift.style.top = "37vw"
-                break
-            case 1:
-                lift.style.top = "21vw"
-                break
-            case 2:
-                lift.style.top = "5vw"
-        }
-    } else if (liftState.moving === direction && liftState.level !== 1) {
-        liftState.queue.push(level)
-        console.log(5000 - time)
-        setTimeout(() => {
-            lift.style.animationPlayState = "paused"
-        }, 5000 - time)
-        setTimeout(() => {
-            lift.style.animationPlayState = "running"
-        }, 5000 - time + 500)
-    }
 }
